1ed17ab107595f7a3e563ff69356bf1a8cac1d65
Ethan Blackwelder <ethan.blackwelder@viterahealthcare.com>
2015-02-25 22:55:29 -0500

    Source-code documentation.

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+/**
+ * Physics engine module.
+ * Exports a "namespace" of mixins.
+ */
+
+/* Kinematics mixins */
+
+//PHYSX.addAcceleration(obj, /*{ x:ax, y:ay }*/ acceleration, /*{ x:vx, y:vy }*/ velocity, /*{ x:x, y:y }*/ startingPosition)
 exports.addAcceleration = require('./lib/mixinAcceleration');
+
+//PHYSX.addVelocity(obj, /*{ x:vx, y:vy }*/ velocity, /*{ x:x, y:y }*/ startingPosition)
 exports.addVelocity = require('./lib/mixinVelocity');
+
+
+/* Bounding-box and out-of-bounds-flagging mixins */
+
+//PHYSX.addBoundingBox(obj, /*{ width:w, height:h }*/ dimensions, /*{ x:x, y:y }*/ boundsOffset)
 exports.addBoundingBox = require('./lib/mixinBoundingBox');
+
+//PHYSX.addBoundsChecking(obj, /*{ left:l, right:r, bottom:b, top:t }*/ inBounds, /*{ width:w, height:h }*/ dimensions, /*{ x:x, y:y }*/ boundsOffset);
 exports.addBoundsChecking = require('./lib/mixinOutOfBoundsChecking');
-exports.addHSL = require('./lib/mixinHSL');
+
+
+/* Tick functions */
+// The "tick" object: { frames:frameCount, deltaMillis:dm, totalMillis:tm }
+
+//PHYSX.mixin(obj, function onTick(tick) { return isTerminated(obj); })
 exports.mixinTerminator = require('./lib/mixinTerminator');
+
+//PHYSX.mixin(obj, function onTick(tick) {...})
 exports.mixin = require('./lib/mixinUpdater');
+
+
+/* Misc mixins */
+
+//PHYSX.addHSL(obj, huePropName, colorPropName)
+exports.addHSL = require('./lib/mixinHSL');
